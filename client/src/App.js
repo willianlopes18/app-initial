1be769ba464b40056c04755d42b22793479b6586
willian <willian.lopes@hotmail.com>
2018-09-18 11:55:02 -0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    token:''
+  }
+
+  componentDidMount(){
+    fetch('/user')
+    .then((res) => res.text())
+    .then(tk => this.setState({token:tk}))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.token}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+app.get('/user', (req,res) => {
+    let resposta = fetch('https://gruponewtech.com.br/inc/usuario/login',{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'convenio-app': '68b385bc7b9a40a0d2981e3d9ba45126'
+        },
+        mode: 'cors',
+        body:JSON.stringify({
+            user:{
+                "login":"willian.lopes@hotmail.com",
+                "senha":"123"
+            }
+        })
+    })
+    .then(res => res.json())
+    .then(token => res.send(token))
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
